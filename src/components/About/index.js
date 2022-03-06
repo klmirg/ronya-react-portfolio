dce9a1cfd9bcb581b97b2657c45d6a5b079a0281
Ronya <klmirg6@gmail.com>
2022-03-06 14:52:37 -0600

    updated the about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,12 @@ function About() {
   return (
     <section className='about'>
       <img className="myImage" src={require(`../../assets/images/ImageOfRonya.jpg`)} alt="Ronya Gettis by a body of water" />
-      <p id="about">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, ab voluptatem facilis ipsa dolor culpa facere vel ducimus est unde expedita consequuntur nostrum quam corporis nam repellat magni doloribus quos!</p>
+      <p id="about">Hi! My name is Ronya Gettis, I am a recent graduate of the University of Minnesota Coding Bootcamp where I earned 
+      a certificate of completion. I am aspiring to become a full stack web developer. I find programming languages and what they can 
+      accomplish to be absolutely fascinating. I come from a background in customer service, with 15 years of experience working in restaurants,
+      with many different job titles throughout those years. I love continuing to learn and develop new skills, wherever I am. I am a team player
+      with a natural ability to adapt to diverse and fast paced environments. I love problem solving which is what led me to my interested in learning
+      Web Development. I would love a chance to broaden my knowledge and continue growing and learning in this field! </p>
     </section>
     
   );
